4a799c8fde01af4e98c2fe97bb50fec3d4a0c92c
Eduardo Soares <edurbs@gmail.com>
2023-12-09 13:17:43 -0300

    add category entity

---
+++ src/main/java/com/edurbs/bill/api/billapi/model/Category.java
@@ -0,0 +1,25 @@
+package com.edurbs.bill.api.billapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "category")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+
+}
