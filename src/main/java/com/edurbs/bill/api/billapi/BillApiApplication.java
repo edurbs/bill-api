7ac57d41fffe79b27b5365fcde9b4478b637e152
Eduardo Soares <edurbs@gmail.com>
2023-12-09 00:39:12 -0300

    add mysql

---
+++ src/main/java/com/edurbs/bill/api/billapi/BillApiApplication.java
@@ -0,0 +1,13 @@
+package com.edurbs.bill.api.billapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillApiApplication.class, args);
+	}
+
+}
