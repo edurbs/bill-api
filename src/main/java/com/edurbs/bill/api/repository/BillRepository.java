57f454de200b9cbc81d66081def9633aadc06215
Eduardo Soares <edurbs@gmail.com>
2023-12-11 11:54:29 -0300

    add bill repository

--- src/main/java/com/edurbs/bill/api/repository/BillRepository.java
+++ src/main/java/com/edurbs/bill/api/repository/BillRepository.java
@@ -3,9 +3,9 @@ package com.edurbs.bill.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.edurbs.bill.api.model.Category;
+import com.edurbs.bill.api.model.Bill;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long>{
- 
+public interface BillRepository extends JpaRepository<Bill, Long> {
+
 }
