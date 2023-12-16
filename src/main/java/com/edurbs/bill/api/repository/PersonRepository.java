6bd0e83e3c1fd1780494e868503f5f6889e962c7
Eduardo Soares <edurbs@gmail.com>
2023-12-15 21:54:57 -0300

    add search persons

--- src/main/java/com/edurbs/bill/api/repository/PersonRepository.java
+++ src/main/java/com/edurbs/bill/api/repository/PersonRepository.java
@@ -1,11 +1,16 @@
 package com.edurbs.bill.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edurbs.bill.api.model.Person;
 
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Page<Person> findByNameContaining(String name, Pageable pageable);    
 
 }
